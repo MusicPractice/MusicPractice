1e5a7e854d6fc0f4c384fd4cefd91834954a1813
littlefean <2028140990@qq.com>
2023-09-11 23:08:09 +0800

    :sparkles: 增加了icon和备案

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -11,6 +11,8 @@
       <p>分享个人创作的音乐作品 | 乐理学习笔记 | 您可以在此训练音乐能力</p>
       <p>站长油箱：littlefean at bitmountain dot top</p>
       <a href="https://beian.miit.gov.cn/" target="_blank">冀ICP备2022004336号-3</a>
+      <br>
+      <a href="https://www.beian.gov.cn/portal/registerSystemInfo?recordcode=13010202003473" target="_blank">冀公安网备 13010202003473</a>
     </div>
   </div>
 </template>
